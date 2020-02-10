a8d403058479791d7aed58afb62ad8c0f04eb8a0
Patrick Tone <hello@patrickt.one>
2020-02-09 22:58:32 -0600

    Updates

---
+++ precache-manifest.108a7ceca78f3a585cb0beeeeed22781.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6032d335f9df4bbb497bd29b4566845",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d69cac8d8c2a9b61f15",
+    "url": "/static/js/2.7d909173.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.7d909173.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15c514836e962f3a12bc",
+    "url": "/static/js/main.512cf66c.chunk.js"
+  },
+  {
+    "revision": "ce54ba44d8492bd9fd1a",
+    "url": "/static/js/runtime-main.28a13f17.js"
+  }
+]);
